5b62d0d9dbd89fef1215124790e57f411a0d7bab
infiduk <infi_duk@naver.com>
2019-09-09 12:22:38 +0900

    update front directory

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Data from './Data';
+
+function App() {
+  return (
+    <div>
+      <Data />
+    </div>
+  );
+}
+
+export default App;
