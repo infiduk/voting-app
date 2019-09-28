4de168a98a781dae7ced05f181f94e5d84b6e5e7
infiduk <201912720@office.induk.ac.kr>
2019-09-28 13:55:51 +0000

    update voting git add .!

--- front/src/Voting.js
+++ front/src/Voting.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Button, Card, Form, FormControl } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
+import List from 'react-list-select';
+import update from 'react-addons-update';
+import values from 'lodash/values'
 
 import Navbar from './Navbar';
 
@@ -10,31 +13,31 @@ export default class Voting extends Component {
             voteId: this.props.match.params.voteId,
             vote: [],
             candidate: [],
-            isClicked: [
-                {
-                    candidateId: '',
-                    clicked: false
-                }
-            ]
+            ingLimit: 0,
+
+            canList: [],
+            canArray: [],
+            status: false,
         };
-        this.handleCheck = this.handleCheck.bind(this);
+        this.handleChecked = this.handleChecked.bind(this);
     }
 
-    handleCheck(e) {
-        alert(e.currentTarget.dataset.id);
-    }
+    // handleCheck(e) {
+    //     alert(e.currentTarget.dataset.id);
+    // }
 
-    async componentDidMount() {
+    componentDidMount() {
+        this._isMounted = true;
         try {
-            await this.callApi();
+            this.callApi();
         } catch (err) {
             console.log(err);
         }
     }
 
-    callApi = async () => {
+    callApi = () => {
         try {
-            await fetch('/vote', {
+            fetch('/vote', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -43,13 +46,18 @@ export default class Voting extends Component {
                     'vote_id': this.state.voteId
                 })
             })
-                .then(result => result.json())
-                .then(json => {
-                    this.setState({ vote: json.voteData, candidate: json.candidateData });
-                })
-                .catch(err => {
-                    console.log(err);
+            .then(result => result.json())
+            .then(json => {
+                this.setState({ vote: json.voteData, candidate: json.candidateData });
+                this.state.candidate.map((c) => {
+                    this.setState({
+                        canList: update(this.state.canList, { $push: [c.name + c.name_ex] })
+                    })
                 });
+            })
+            .catch(err => {
+                console.log(err);
+            });
         } catch (err) {
             console.log(err);
         }
@@ -64,21 +72,54 @@ export default class Voting extends Component {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    'votd_id': this.state.vote_id,
+                    'vote_id': this.state.canArray,
                     'candidates': this.state.candidate
                 })
             })
-                .then(result => result.json())
-                .then(json => {
-                    this.setState({ vote: json.voteData, candidate: json.candidateData });
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+            .then(result => result.json())
+            .then(json => {
+                console.log(json.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
         } catch (err) {
             console.log(err);
         }
     };
+
+    handleChecked = async (selected) => {
+        let indexId;
+        if(selected.length > 1) {
+            indexId = selected[selected.length - 1];
+        } else {
+            indexId = selected[0];
+        }
+        try {
+            this.state.canArray.map((elem, index) => {
+                if (elem === indexId) {
+                    this.state.ingLimit -= 1;
+                    this.state.status = true;
+                    this.setState({
+                        canArray: update(this.state.canArray, { $splice: [[index, 1]] })
+                    })
+                }
+            });
+            if(!this.state.status) {
+                if (this.state.ingLimit < this.state.vote.limit) {
+                    this.state.ingLimit += 1;
+                    this.setState({
+                        canArray: update(this.state.canArray, { $push: [indexId] }),
+                     })
+                } else {
+                    console.log('full');
+                }
+            }
+            this.state.status = false;
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     render() {
         return (
@@ -86,27 +127,14 @@ export default class Voting extends Component {
                 <Navbar />
                 <div style={{ margin: 25 }}>
                     <h3>{this.state.vote.title}</h3>
-                    {/* 자동으로 따라다니는 애로 넣기 */}
-                    <h4 style={{ margin: 10 }}>투표 인원 : 몇명 / 몇명</h4>
-                    <Form inline style={{ marginBottom: 10 }}>
-                        <FormControl type='text' placeholder='검색할 후보자의 이름을 입력하세요.' className='mr-sm-2' style={{ width: '70%' }} />
-                        <Button variant='primary'>검색</Button>
-                    </Form>
+                    <h4 style={{ margin: 10 }}>선택한 후보자 : {this.state.ingLimit}명 / {this.state.vote.limit}명</h4>
                     <div className='card' style={{ padding: '5px', backgroundColor: '#fafafa' }}>
-                        {this.state.candidate.map(candidate => {
-                            return <Card className='card' style={{ height: '4rem', margin: 3, backgroundColor: '#fff' }} onClick={this.handleCheck} key={candidate.id} data-id={candidate.id}>  
-                                <Card.Body>
-                                    <Form.Check inline id={candidate.id} /><Card.Title>{candidate.name} {candidate.name_ex}
-                                    </Card.Title>
-                                </Card.Body>
-                            </Card>
-                        })}
+                        <List
+                        items={this.state.canList}
+                        multiple={true}
+                        onChange={(selected: number) => { this.handleChecked(selected)}}
+                    />
                     </div>
-                    {/*  return <Card className='card' style={{ height: '4rem', margin: 3, backgroundColor: '#fff' }} onClick={this.handleCheck} key={candidate.id} data-id={candidate.id}>
-                                <Card.Body>
-                                    <Card.Title>{candidate.name} {candidate.name_ex}</Card.Title>
-                                </Card.Body>
-                            </Card> */}
                     <Form>
                         <Button variant='primary' size='lg' style={{ marginTop: 25 }} block onClick={this.handleVoteSubmit}>확인</Button>
                     </Form>
